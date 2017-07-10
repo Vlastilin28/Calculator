5dfd42547bee5d47094607da008a79d2f5094ec6
Vlastilin28 <nic.kz@mail.ru>
2017-07-10 16:06:14 +0600

    First working version

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_plus = new System.Windows.Forms.Button();
+            this.button_minus = new System.Windows.Forms.Button();
+            this.button_multiply = new System.Windows.Forms.Button();
+            this.button_divide = new System.Windows.Forms.Button();
+            this.textBox_enterone = new System.Windows.Forms.TextBox();
+            this.textBox_entertwo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_plus
+            // 
+            this.button_plus.Location = new System.Drawing.Point(50, 133);
+            this.button_plus.Name = "button_plus";
+            this.button_plus.Size = new System.Drawing.Size(75, 23);
+            this.button_plus.TabIndex = 0;
+            this.button_plus.Text = "+";
+            this.button_plus.UseVisualStyleBackColor = true;
+            this.button_plus.Click += new System.EventHandler(this.Button_click);
+            // 
+            // button_minus
+            // 
+            this.button_minus.Location = new System.Drawing.Point(131, 133);
+            this.button_minus.Name = "button_minus";
+            this.button_minus.Size = new System.Drawing.Size(75, 23);
+            this.button_minus.TabIndex = 1;
+            this.button_minus.Text = "-";
+            this.button_minus.UseVisualStyleBackColor = true;
+            this.button_minus.Click += new System.EventHandler(this.Button_click);
+            // 
+            // button_multiply
+            // 
+            this.button_multiply.Location = new System.Drawing.Point(50, 162);
+            this.button_multiply.Name = "button_multiply";
+            this.button_multiply.Size = new System.Drawing.Size(75, 23);
+            this.button_multiply.TabIndex = 2;
+            this.button_multiply.Text = "*";
+            this.button_multiply.UseVisualStyleBackColor = true;
+            this.button_multiply.Click += new System.EventHandler(this.Button_click);
+            // 
+            // button_divide
+            // 
+            this.button_divide.Location = new System.Drawing.Point(131, 162);
+            this.button_divide.Name = "button_divide";
+            this.button_divide.Size = new System.Drawing.Size(75, 23);
+            this.button_divide.TabIndex = 3;
+            this.button_divide.Text = "/";
+            this.button_divide.UseVisualStyleBackColor = true;
+            this.button_divide.Click += new System.EventHandler(this.Button_click);
+            // 
+            // textBox_enterone
+            // 
+            this.textBox_enterone.Location = new System.Drawing.Point(50, 22);
+            this.textBox_enterone.Name = "textBox_enterone";
+            this.textBox_enterone.Size = new System.Drawing.Size(100, 20);
+            this.textBox_enterone.TabIndex = 4;
+            // 
+            // textBox_entertwo
+            // 
+            this.textBox_entertwo.Location = new System.Drawing.Point(50, 48);
+            this.textBox_entertwo.Name = "textBox_entertwo";
+            this.textBox_entertwo.Size = new System.Drawing.Size(100, 20);
+            this.textBox_entertwo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 243);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_entertwo);
+            this.Controls.Add(this.textBox_enterone);
+            this.Controls.Add(this.button_divide);
+            this.Controls.Add(this.button_multiply);
+            this.Controls.Add(this.button_minus);
+            this.Controls.Add(this.button_plus);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_plus;
+        private System.Windows.Forms.Button button_minus;
+        private System.Windows.Forms.Button button_multiply;
+        private System.Windows.Forms.Button button_divide;
+        private System.Windows.Forms.TextBox textBox_enterone;
+        private System.Windows.Forms.TextBox textBox_entertwo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,5 +16,31 @@ namespace Calculator
         {
             InitializeComponent();
         }
+        private void Button_click(object sender, EventArgs e)
+        {
+            double numberone = Convert.ToDouble(textBox_enterone.Text);
+            double numbertwo = Convert.ToDouble(textBox_entertwo.Text);
+            double result;
+            switch (((Button)sender).Name)
+            {
+                case "button_plus":
+                    result = numberone + numbertwo;
+                    break;
+                case "button_minus":
+                    result = numberone - numbertwo;
+                    break;
+                case "button_multiply":
+                    result = numberone * numbertwo;
+                    break;
+                case "button_divide":
+                    result = numberone / numbertwo;
+                    
+                    break;
+                default:
+                    throw new Exception("Неизвестная операция");
+            }
+
+            label1.Text = Convert.ToString(result);
+        }
     }
 }
